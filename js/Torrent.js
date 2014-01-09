603431f9e3cdffa0a77b640adbe1fb7c9c118063
tghosgor <fasdfasdas@gmail.com>
2014-01-09 03:03:01 +0200

    named basic Torrent object functions

--- js/Torrent.js
+++ js/Torrent.js
@@ -7,3 +7,19 @@ Torrent.prototype.size = function()
 {
   return (this.data.info.pieces.length * this.data.info["piece length"] / 20);
 }
+
+Torrent.prototype.start = function()
+{
+}
+
+Torrent.prototype.stop = function()
+{
+}
+
+Torrent.prototype.status = function()
+{
+}
+
+Torrent.prototype.completionPercentage = function()
+{
+}
